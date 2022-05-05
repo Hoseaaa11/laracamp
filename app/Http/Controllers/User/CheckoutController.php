239f7a6e034e4eafc10c2fe99f9dc04b1711dffa
Hosea <hoseaabawono@gmail.com>
2022-05-05 17:58:21 +0700

    Validation

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\checkout;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
 
@@ -25,8 +26,14 @@ class CheckoutController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Camp $camp)
+    public function create(Camp $camp, request $request)
     {
+
+    if ($camp -> isRegistered){
+        $request->session()->flash('error', "You already registered on {$camp->title} camp.");
+        return redirect(route('dashboard'));
+    }
+
     return view('checkout.create', [
         'camp' => $camp
     ]);
@@ -39,8 +46,9 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {   
+      
         // mapping request data
         $data = $request->all();
         $data['user_id'] = Auth::id();
@@ -51,7 +59,7 @@ class CheckoutController extends Controller
         $user -> email = $data['email'];
         $user -> name = $data['name'];
         $user -> occupation = $data['occupation'];
-        $user->save();    
+        $user->save();
 
         //create checkout
         $checkout = Checkout::create($data);
